d7b188f15c2880641b3ee26d13a01baaa6591e2a
zrogers010 <zwilliamrogers@gmail.com>
2024-05-12 13:43:28 -0700

    chanlink real world asset
    Tokenized tesla stock rwa asset example. Chainlink blockchain developer.

---
+++ src/dTSLA.sol
@@ -0,0 +1,207 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+import {FunctionsClient} from "@chainlink/contracts/src/v0.8/functions/dev/v1_0_0/FunctionsClient.sol";
+import {ConfirmedOwner} from "@chainlink/contracts/src/v0.8/shared/access/ConfirmedOwner.sol";
+import {FunctionsRequest} from "@chainlink/contracts/src/v0.8/functions/dev/v1_0_0/libraries/FunctionsRequest.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import {Strings} from "@openzeppelin/contracts/utils/Strings.sol";
+/*
+ * @title dTSLA
+ * @author Z
+ */
+contract dTSLA is ConfirmedOwner, FunctionsClient, ERC20 {
+    using FunctionsRequest for FunctionsRequest.Request;
+
+    error dTSlA__NotEnoughCollateral();
+    error dTSLA__DoesntMeetMinimumWithdrawalAmount();
+    error dTSLA_TransferFailed();
+
+    enum MintOrRedeem {
+        mint,
+        redeem
+    }
+
+    struct dTslaRequest {
+        uint256 amountOfToken;
+        address requester;
+        MintOrRedeem mintOrRedeem;
+    }
+
+    // Constants
+    address constant SEPOLIA_FUNCTIONS_ROUTER = 0x65Dcc24F8ff9e51F10DCc7Ed1e4e2A61e6E14bd6;
+    bytes32 constant DON_ID = 0x66756e2d657468657265756d2d6d61696e6e65742d3100000000000000000000;
+    address constant SEPOLIA_TSLA_PRICE_FEED = 0xc59E3633BAAC79493d908e63626716e204A45EdF; // LINK/USD feed for demo purposes
+    address constant SEPOLIA_USDC_PRICE_FEED = 0xA2F78ab2355fe2f984D808B5CeE7FD0A93D5270E; // USDC/USD
+    address constant SEPOLIA_USDC = 0xAF0d217854155ea67D583E4CB5724f7caeC3Dc87; 
+    // Sepolia usdc addr: //0x1c7D4B196Cb0C7B01d743Fbc6116a902379C7238
+    uint256 constant ADDITIONAL_FEED_PRECISION = 1e10;
+    uint32 constant GAS_LIMIT = 300_000;
+    uint256 constant COLLATERAL_RATIO = 200; //200% collateral ratio
+    uint256 constant COLLATERAL_PRECISION = 100;
+    uint256 constant MINIMUM_WITHDRAWL_AMOUNT = 100e18;
+
+    uint64 immutable i_subId;
+
+    // Storage Variables
+    string private s_mintSourceCode;
+    string private s_redeemSourceCode;
+    uint256 private s_portfolioBalance;
+    mapping(bytes32 requestId => dTslaRequest request) private s_requestIdToRequest;
+
+    // Functions
+    constructor(string memory mintSourceCode, uint64 subId, string memory redeemSourceCode) 
+        ConfirmedOwner(msg.sender) 
+        FunctionsClient(SEPOLIA_FUNCTIONS_ROUTER) 
+        ERC20("dTSLA", "dTSLA")
+    {
+        s_mintSourceCode = mintSourceCode;
+        i_subId = subId;
+    }
+
+
+    // Send HTTP request to: 
+    // 1) see how much TSLA is bought
+    // 2) mint dTSLA if enough TSLA in bank account
+    // 2-transaction function: send / receive. callback
+    function sendMintRequest(uint256 amount) external onlyOwner returns (bytes32){
+        FunctionsRequest.Request memory req;
+        req.initializeRequestForInlineJavaScript(s_mintSourceCode);
+        bytes32 requestId = _sendRequest(req.encodeCBOR(), i_subId, GAS_LIMIT, DON_ID);
+        s_requestIdToRequest[requestId] = dTslaRequest(amount, msg.sender, MintOrRedeem.mint);
+        return requestId;
+
+    }
+
+    function _mintFulFillRequest(bytes32 requestId, bytes memory response) internal {
+        uint256 amountOfTokensToMint = s_requestIdToRequest[requestId].amountOfToken;
+        s_portfolioBalance = uint256(bytes32(response));
+
+        if(_getCollateralRatioAdjustedTotalBalance(amountOfTokensToMint) > s_portfolioBalance) {
+            revert dTSlA__NotEnoughCollateral();
+        }
+
+        if(amountOfTokensToMint != 0) {
+            _mint(s_requestIdToRequest[requestId].requester, amountOfTokensToMint);
+        }
+    }
+
+    // @notice User sends request to sell tsla for usdc.
+    // Chainlink function will call alpaca and: 
+    // 1) sell tsla
+    // 2) buys usdc
+    // 3) send usdc to this contract
+    function sendRedeemRequest(uint256 amountdTesla) external {
+        uint256 amountTslaInUsdc = getUsdcValueOfUsd(getUsdValueOfTsla(amountdTesla));
+        if (amountTslaInUsdc < MINIMUM_WITHDRAWL_AMOUNT) {
+            revert dTSLA__DoesntMeetMinimumWithdrawalAmount();
+        }
+
+        FunctionsRequest.Request memory req;
+        req.initializeRequestForInlineJavaScript(s_redeemSourceCode);
+        
+        string[] memory args = new string[](2);
+        args[0] = amountdTesla.toString();
+        args[1] = amountTslaInUsdc.toString();
+        req.setArgs(args);
+        
+        bytes32 requestId = _sendRequest(req.encodeCBOR(), i_subId, GAS_LIMIT, DON_ID);
+        s_requestIdToRequest[requestId] = dTslaRequest(amountdTesla, msg.sender, MintOrRedeem.redeem);
+
+        _burn(msg.sender, amountdTesla);
+    }
+
+    function _redeemFulFillRequest(bytes32 requestId, bytes memory response) internal {
+        // assume 18 decimals for now..
+        uint256 usdcAmount = uint256(bytes32(respons));
+        if usdcAmount == 0 {
+            uint256 amountOfdTSLABurned = s_requestIdToRequest[requestId].amountIfToken;
+            _mint(s_requestIdToRequest[requestId].requester, amountOfdTSLABurned);
+            return;
+        }
+
+        s_userToWithdrawlAmount[s_requestIdToRequest[requestId].requester] += usdcAmount;
+    }
+
+    function withdraw() external {
+        uint256 amountToWithdraw = s_userToWithdrawlAmount[msg.sender];
+        s_userToWithdrawlAmount[msg.sender] = 0;
+
+        bool success = ERC20(0xAF0d217854155ea67D583E4CB5724f7caeC3Dc87).transfer(msg.sender, amountToWithdraw);
+        if (!success) {
+            revert dTSLA_TransferFailed();
+        }
+    }
+
+    function fulfillRequest(bytes32 requestId, bytes memory response, bytes memory /*err*/ ) internal override {
+            if (s_requestIdToRequest[requestId].mintOrRedeem == MintOrRedeem.mint) {
+                _mintFulFillRequest(requestId, response);
+            } else {
+                _redeemFulFillRequest(requestId, response);
+            }
+    }
+
+    function _getCollateralRatioAdjustedTotalBalance(uint256 amountOfTokensToMint) internal view returns (uint256) {
+        uint256 calculatedNewTotalValue = getCalculatedNewTotalValue(amountOfTokensToMint);
+        return (calculatedNewTotalValue * COLLATERAL_RATIO) / COLLATERAL_PRECISION;
+    }
+
+    function getCalculatedNewTotalValue(uint256 addedNumberOfTokens) internal view returns (uint256) {
+        return ((totalSupply() + addedNumberOfTokens) * getTslaPrice()) / PRECISION;
+    }
+
+    function getUsdcValueOfUsd(uint256 usdAmount) public view returns (uint256) {
+        return (usdAmount * getUsdcPrice()) / PRECISION;
+    }
+
+    function getUsdValueOfTsla(uint256 tslaAmount) public view returns (uint256) {
+        return (tslaAmount * getTslaPrice()) / PRECISION;
+    }
+
+    function getTslaPrice() public view returns (uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(SEPOLIA_TSLA_PRICE_FEED);
+        (, int256 price,,,) = priceFeed.latestRoundData();
+        return uint256(price) * ADDITIONAL_FEED_PRECISION;
+    }
+
+    function getUsdcPrice() public view returns (uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(SEPOLIA_USDC_PRICE_FEED);
+        (, int256 price,,,) = priceFeed.latestRoundData();
+        return uint256(price) * ADDITIONAL_FEED_PRECISION;
+    }
+
+    // View and Pure Functions
+    function getRequest(bytes32 requestId) public view returns (dTslaRequest memory) {
+        return s_requestIdToRequest[requestId];
+    }
+
+    function getPendingWithdrawlAmount(address user) public view returns (uint256) {
+        return s_portfolioBalance
+    }
+
+    function getPortfolioBalance() public view returns (uint256) {
+        return i_subId;
+    }
+
+    function getSubId() public view returns (uint64) {
+        return i_subId;
+    }
+
+    function getMintSourceCode() public view returns (string memory) {
+        return s_mintSourceCode;
+    }
+
+    function getRedeemSourceCode() public view returns (uint256) {
+        return s_redeemSourceCode;
+    }
+
+    function getCollateralRatio() public pure returns (uint256) {
+        return COLLATERAL_RATIO;
+    }
+
+    function getCollateralPrecision() public pure returns (uint256) {
+        return COLLATERAL_PRECISION;
+    }
+
+}
